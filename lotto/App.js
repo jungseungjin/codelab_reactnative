b2dbb8db7a96971462e80c4793147706ed20c319
jungseungjin <ywack@naver.com>
2020-07-24 23:26:00 +0900

    lotto

---
+++ lotto/App.js
@@ -0,0 +1,115 @@
+import * as React from "react";
+import { Button, SafeAreaView, Text, View, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+import _ from "lodash";
+import styled from "styled-components/native";
+
+let numbers = [];
+//1부터 45까지 숫자 생성 ,
+_.times(45, (n) => numbers.push(n + 1));
+
+//섞는다
+//numbers = _.shuffle(numbers);
+
+const Row = styled.View`
+  flex-direction: row;
+  margin-bottom: 24px;
+`;
+
+const Ball = styled.View`
+  width: 50px;
+  height: 50px;
+  background: ${(props) => {
+    if (props.value < 11) {
+      return "#e5e251";
+    } else if (props.value < 21) {
+      return "#517FE5";
+    } else if (props.value < 31) {
+      return "#E54036";
+    } else if (props.value < 41) {
+      return "#e5e5e5";
+    } else {
+      return "#43BF74";
+    }
+  }};
+  border-radius: 25px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Label = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #000000;
+`;
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding-top: ${Constants.statusBarHeight}px;
+`;
+export default function App() {
+  //상태 변화 state
+  const [displayNumbers, setNumbers] = React.useState(_.shuffle(numbers));
+  //displayNumbers - 상태의 읽기 전용 값
+  //setNumbers - 상태를 변경하기 위한 업데이트 함수
+
+  //react fragment <></>  실제 렌더링에 반영되지 않는 추상 컨테이너
+  return (
+    <Container>
+      <Row>
+        <Ball color={"#e5e5e5"} value={displayNumbers[0]}>
+          <Label>{displayNumbers[0]}</Label>
+        </Ball>
+        <Ball color={"#cccccc"} value={displayNumbers[1]}>
+          <Label>{displayNumbers[1]}</Label>
+        </Ball>
+        <Ball value={displayNumbers[2]}>
+          <Label>{displayNumbers[2]}</Label>
+        </Ball>
+        <Ball value={displayNumbers[3]}>
+          <Label>{displayNumbers[3]}</Label>
+        </Ball>
+        <Ball value={displayNumbers[4]}>
+          <Label>{displayNumbers[4]}</Label>
+        </Ball>
+        <Ball value={displayNumbers[5]}>
+          <Label>{displayNumbers[5]}</Label>
+        </Ball>
+      </Row>
+      <Button
+        title="다시 뽑기"
+        onPress={() => {
+          setNumbers(_.shuffle(numbers));
+        }}
+      ></Button>
+    </Container>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  row: {
+    flexDirection: "center",
+    marginBottom: 24,
+  },
+  ball: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "#e5e5e5",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    color: "#000000",
+    fontWeight: "bold",
+  },
+});
