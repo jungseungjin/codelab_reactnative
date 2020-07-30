2c2cb7a4381c68006b0f1beb4a970a8d196378cd
jungseungjin <ywack@naver.com>
2020-07-30 14:17:00 +0900

    초성함수

---
+++ getInitials.js
@@ -0,0 +1,10 @@
+function getInitials(string) {
+  return string
+    .split("")
+    .map((char) => {
+      const index = (char.charCodeAt(0) - 44032) / 28 / 21;
+      if (index >= 0) return String.fromCharCode(index + 4352);
+      return char;
+    })
+    .join("");
+}
